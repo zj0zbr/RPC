95a28ebeba490950e6637564a1fbbf88405ea5ae
zj0zbr <m13752362332_1@163.com>
2017-08-15 22:18:08 +0800

    Update IDosomething.java

--- IDosomething.java
+++ IDosomething.java
@@ -1,10 +1,10 @@
-package org.socket.rpc.idl;
+package zbr.socket.rpc.idl;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- * Created by andilyliao on 17-4-9.
+ * Created by zj0zbr on 17-4-9.
  */
 public abstract class IDosomething {
     public String doSomething(Method m,String param) throws InvocationTargetException, IllegalAccessException {
