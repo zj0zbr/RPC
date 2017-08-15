cf5b2f2927f98659871c0957b17efcda1f5bf53e
zj0zbr <m13752362332_1@163.com>
2017-08-15 22:16:56 +0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,16 +1,16 @@
-package org.socket.rpc.idl;
+package zbr.socket.rpc.idl;
 
-import org.socket.rpc.framework.Send;
+import zbr.socket.rpc.framework.Send;
 
 import java.io.IOException;
 
 /**
- * Created by andilyliao on 17-4-9.
+ * Created by zj0zbr on 17-4-9.
  */
 public class Client {
     public boolean login(String ip,int port,String username,String password) throws IOException {
         Send send=new Send(ip,port);
-        String res=send.sendsomething("org.socket.rpc.idl.ILogin|login|"+username+"|"+password);
+        String res=send.sendsomething("zbr.socket.rpc.idl.ILogin|login|"+username+"|"+password);
         switch (res){
             case "TRUE":
                 return true;
